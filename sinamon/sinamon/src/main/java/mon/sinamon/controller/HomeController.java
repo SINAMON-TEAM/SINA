eb3835451725cd1de3799132d3b3b1a9494ad2d2
the-groot <sangwoo0795@naver.com>
2022-07-18 21:50:33 +0900

    웰컴페이지 만듬

---
+++ sinamon/sinamon/src/main/java/mon/sinamon/controller/HomeController.java
@@ -0,0 +1,15 @@
+package mon.sinamon.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String hello(Model model) {
+        return "home";
+    }
+}
