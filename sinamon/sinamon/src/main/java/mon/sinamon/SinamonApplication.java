e5ff5307016fed1bb8476e8cdc35d2b41a08b44d
the-groot <sangwoo0795@naver.com>
2022-07-18 20:55:48 +0900

    create spring project

---
+++ sinamon/sinamon/src/main/java/mon/sinamon/SinamonApplication.java
@@ -0,0 +1,13 @@
+package mon.sinamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinamonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinamonApplication.class, args);
+	}
+
+}
---
+++ sinamon/sinamon/src/test/java/mon/sinamon/SinamonApplicationTests.java
@@ -0,0 +1,13 @@
+package mon.sinamon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinamonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
