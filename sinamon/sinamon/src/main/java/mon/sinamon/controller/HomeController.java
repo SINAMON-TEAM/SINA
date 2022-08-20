29e0e8c4294dce6557cdb61800be555957ddd185
JeonghyeonK <6843wjdgus@naver.com>
2022-08-21 02:39:46 +0900

    간단한 채팅 기능 개발

--- sinamon/sinamon/src/main/java/mon/sinamon/controller/HomeController.java
+++ sinamon/sinamon/src/main/java/mon/sinamon/controller/HomeController.java
@@ -4,12 +4,10 @@ package mon.sinamon.controller;
 import mon.sinamon.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin("*") // 모든 요청에 접근 허용
 public class HomeController {
     @GetMapping("/")
     public String hello(Model model) {
